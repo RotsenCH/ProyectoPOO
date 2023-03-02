5babd7ac4cd7e19cf0454d6e7c5d1e2c4c2b0a98
DannyVinueza <danny.vinueza@epn.edu.ec>
2023-03-01 19:52:34 -0500

    Panalla cajero.

---
+++ src/Cajero.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+
+public class Cajero {
+    private JPanel cajero;
+    private JButton regresarL;
+    private JPanel panel1;
+    private JTextField buscarP;
+    private JButton buscar;
+    private JTextField nomProd;
+    private JTextField precioProd;
+    private JSpinner cantidad;
+    private JButton agregar;
+    private JButton eliminar;
+    private JTable table1;
+    private JTable table2;
+    private JButton FACTURARButton;
+    private JButton CLIENTEButton;
+    private JLabel mensajeTXT;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}

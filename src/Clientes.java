74ee5da2aa231ffcee64e74bcef921a455d78582
jp123468 <jose.panchi@epn.edu.ec>
2023-03-01 20:22:29 -0500

    form de clientes

---
+++ src/Clientes.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+
+public class Clientes {
+    private JPanel clientes1;
+    private JTextField textField1;
+    private JButton BuscarCedCli;
+    private JButton EliminarCli;
+    private JButton AgregarCli;
+    private JTextField textFieldNom_Cli;
+    private JTextField textFieldApell_Cli;
+    private JTextField textFieldCorreo_Cli;
+    private JTextField textFieldTlf_Cli;
+    private JTextField textFieldDirec_Cli;
+    private JButton ModificarCli;
+    private JButton AceptarCli;
+    private JButton RegresarCaja;
+    private JLabel Mensajelabel;
+    private JPanel clientes2;
+
+
+}

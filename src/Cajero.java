6e45c1c8850179b87fbf11b2b3b11f02d8dcd0e8
Nestor <nestor.chumania@epn.edu.ec>
2023-03-02 01:04:51 -0500

    Edicion de Jframe Cajero

--- src/Cajero.java
+++ src/Cajero.java
@@ -4,17 +4,13 @@ public class Cajero extends Login {
     public JPanel cajero_panel;
     private JButton regresarL;
     private JPanel panel1;
-    private JTextField buscarP;
     private JButton buscar;
-    private JTextField nomProd;
-    private JTextField precioProd;
     private JSpinner cantidad;
     private JButton agregar;
     private JButton eliminar;
     private JTable table1;
     private JTable table2;
-    private JButton FACTURARButton;
-    private JButton CLIENTEButton;
+    private JTextField textField1;
     private JLabel mensajeTXT;
 
     private void createUIComponents() {
